05521634b05b22baa7b415a5130a8a5053f6c6e0
Vanshul <vanshulg101@gmail.com>
2026-06-28 02:11:07 +0530

    fix: Add basename support to React Router for subdirectory hosting

--- src/App.jsx
+++ src/App.jsx
@@ -190,7 +190,7 @@ function App() {
   };
 
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <ScrollToHash isBooting={isBooting} />
       <ToastProvider>
         <GlobalStyles />
