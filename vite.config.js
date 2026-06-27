5981cc9336b01b0dbc60a8959d88807483e84ef7
Vanshul <vanshulg101@gmail.com>
2026-06-28 02:08:52 +0530

    fix: Set base path to /urvin.github.io/ for GitHub Pages assets

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/urvin.github.io/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
